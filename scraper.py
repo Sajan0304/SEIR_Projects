a258d6cd7b7b933c64804880e29723ca01c52a48
Sajan Kumar <kumarsajan0304@gmail.com>
2026-03-03 13:32:02 +0530

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,44 @@
+import sys
+from selenium import webdriver
+import time
+from bs4 import BeautifulSoup
+
+
+def get_soup(page_url):
+    driver = webdriver.Chrome()
+    driver.get(page_url)
+    time.sleep(5)
+    return BeautifulSoup(driver.page_source, 'lxml')
+
+def get_title_text(s):
+    if s.title:
+        return s.title.text.strip()
+    else:
+        return ""
+    
+
+def get_body_text(s):
+    
+    if s.body:
+        return s.body.get_text()
+    else:
+        return ""
+
+
+def get_all_urls(s):
+    urls=[]
+    for link in s.find_all('a'):
+        href = link.get('href')
+        if href:
+            urls.append(href)
+
+    return urls
+
+page_url = sys.argv[1]
+s=(get_soup(page_url))
+print(get_title_text(s))
+print(get_body_text(s))
+
+for url in get_all_urls(s):
+    print(url)
+    
